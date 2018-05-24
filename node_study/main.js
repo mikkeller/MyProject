6c85529f0dcf070f68cd6ff4e4d4574afcd7cc8a
Mikkeller <mycarton@naver.com>
2018-05-24 17:19:57 +0900

    node 시작

---
+++ node_study/main.js
@@ -0,0 +1,3 @@
+const other = require('./other'); // 같은 경로의 other를 require로 가져온다.
+
+console.log(other());
--- node_study_project/main.js
+++
@@ -1,3 +0,0 @@
-const other = require('./other'); // 갖은 경로의 other를 require로 가져온다.
-
-console.log(other());
